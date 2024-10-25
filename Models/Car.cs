ad9d7ad6b3343ed0b63362cccae462fedb3d3e25
Sabuhi <sabuhies-ab108@code.edu.az>
2024-10-25 12:33:54 +0400

    repo created

---
+++ Models/Car.cs
@@ -0,0 +1,40 @@
+﻿namespace CarsAndStuff.Models;
+
+class Car
+{
+    public int Id { get; set; }
+    public int Year { get; set; }
+    public int EngineCode { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+
+    public static bool operator <(Car car1, Car car2)
+    {
+        return car1.Year < car2.Year;
+    }
+    public static bool operator >(Car car1, Car car2)
+    {
+        return car1.Year > car2.Year;
+    }
+}
+
+static class SalamAleykumAllahSiziQorusun
+{
+    public static void OrderByYear(this Car[] cars)
+    {
+        Car[] newArr = new Car[cars.Length];
+        Car mem = cars[0];
+        for (int i = 0; i < cars.Length; i++)
+        {
+            for (int j = 0; j < cars.Length; j++)
+            {
+                if (cars[j] < mem && !cars.Contains(cars[j]))
+                {
+                    mem = cars[j];
+                }
+            }
+            newArr[i] = mem;
+        }
+        cars = newArr;
+    }
+}
